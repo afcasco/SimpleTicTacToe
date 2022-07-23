2b0a6debcfc47ed9f11d1bdccd4416142d04dae2
afcasco <afcasco@gmail.com>
2022-07-23 14:08:30 +0200

    jetbrains acadaemy tictactoe 4/5 ok

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final int ROWS = 3;
+    private static final int COLS = 3;
     private static final String[] POSSIBLE_RESULTS = {"X wins", "O wins", "Impossible", "Draw", "Game not finished"};
     private static final int X_WINS = 0;
     private static final int O_WINS = 1;
@@ -12,58 +14,82 @@ public class Main {
     private static final int NOT_FINISHED = 4;
 
     public static void main(String[] args) {
-        String[] game = getGame();
+
+        String[][] game = getGame();
+
         printDashes();
         printGame(game);
         printDashes();
-        System.out.println(getGameResult(game));
+
+        newUserMove(game);
+
+        printDashes();
+        printGame(game);
+        printDashes();
+        // System.out.println(getGameResult(game));
     }
 
-    public static String getGameResult(String[] game) {
+    private static void newUserMove(String[][] game) {
+        boolean valid = false;
+        int[] userInput;
+        do {
+            userInput = getUserInput();
+            if (game[userInput[0]][userInput[1]].trim().isEmpty() || game[userInput[0]][userInput[1]].equals("_")) {
+                valid = true;
+                game[userInput[0]][userInput[1]] = "X";
+            } else {
+                System.out.println("This cell is occupied! Choose another one!");
+            }
+        } while (!valid);
+
+    }
+
+    public static String getGameResult(String[][] game) {
 
         int countX = 0;
         int countO = 0;
         String result = null;
         int numOfWinners = 0;
 
-        for (String x : game) {
-            if (x.equalsIgnoreCase("O")) countO++;
-            if (x.equalsIgnoreCase("X")) countX++;
+        for (String[] x : game) {
+            for (String z : x) {
+                if (z.equalsIgnoreCase("O")) countO++;
+                if (z.equalsIgnoreCase("X")) countX++;
+            }
         }
 
-        if (game[0].equalsIgnoreCase(game[1]) && game[0].equalsIgnoreCase(game[2])) {
-            result = game[0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[0][0].equalsIgnoreCase(game[0][1]) && game[0][0].equalsIgnoreCase(game[0][2])) {
+            result = game[0][0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-        if (game[3].equalsIgnoreCase(game[4]) && game[3].equalsIgnoreCase(game[5])) {
-            result = game[3].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[1][0].equalsIgnoreCase(game[1][1]) && game[1][0].equalsIgnoreCase(game[1][2])) {
+            result = game[1][0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-        if (game[6].equalsIgnoreCase(game[7]) && game[6].equalsIgnoreCase(game[8])) {
-            result = game[6].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[2][0].equalsIgnoreCase(game[2][1]) && game[2][0].equalsIgnoreCase(game[2][2])) {
+            result = game[2][0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-        if (game[0].equalsIgnoreCase(game[4]) && game[0].equalsIgnoreCase(game[8])) {
-            result = game[0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[0][0].equalsIgnoreCase(game[1][1]) && game[0][0].equalsIgnoreCase(game[2][2])) {
+            result = game[0][0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-        if (game[2].equalsIgnoreCase(game[4]) && game[2].equalsIgnoreCase(game[6])) {
-            result = game[2].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[0][2].equalsIgnoreCase(game[1][1]) && game[0][2].equalsIgnoreCase(game[2][0])) {
+            result = game[0][2].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-        if (game[0].equalsIgnoreCase(game[3]) && game[0].equalsIgnoreCase(game[6])) {
-            result = game[0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[0][0].equalsIgnoreCase(game[1][0]) && game[0][0].equalsIgnoreCase(game[2][0])) {
+            result = game[0][0].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-        if (game[1].equalsIgnoreCase(game[4]) && game[1].equalsIgnoreCase(game[7])) {
-            result = game[1].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[0][1].equalsIgnoreCase(game[1][1]) && game[0][1].equalsIgnoreCase(game[2][1])) {
+            result = game[0][1].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-        if (game[2].equalsIgnoreCase(game[5]) && game[2].equalsIgnoreCase(game[8])) {
-            result = game[2].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
+        if (game[0][2].equalsIgnoreCase(game[1][2]) && game[0][2].equalsIgnoreCase(game[2][2])) {
+            result = game[0][2].equalsIgnoreCase("X") ? POSSIBLE_RESULTS[X_WINS] : POSSIBLE_RESULTS[O_WINS];
             numOfWinners++;
         }
-
         if (numOfWinners == 0) {
             result = POSSIBLE_RESULTS[DRAW];
         }
@@ -73,7 +99,6 @@ public class Main {
         if (Math.abs(countO - countX) >= 2 || numOfWinners > 1) {
             result = POSSIBLE_RESULTS[IMPOSSIBLE];
         }
-
         return result;
     }
 
@@ -81,27 +106,66 @@ public class Main {
         System.out.println("---------");
     }
 
-    public static void printGame(String[] game) {
-        for (int i = 0; i < game.length; i++) {
-            if (i % 3 == 0) {
-                System.out.print("| ");
+    public static void printGame(String[][] game) {
+        for (String[] line : game) {
+            System.out.print("| ");
+            for (String position : line) {
+                System.out.print(position + " ");
             }
-            System.out.print(game[i] + " ");
-            if ((i + 1) % 3 == 0) {
-                System.out.println("|");
-            }
+            System.out.println("|");
         }
     }
 
-    public static String[] getGame() {
+    public static String[][] getGame() {
         Scanner scanner = new Scanner(System.in);
         String symbols = scanner.nextLine();
-        String[] game = new String[symbols.length()];
+        String[][] game = new String[ROWS][COLS];
 
-        for (int i = 0; i < symbols.length(); i++) {
-            game[i] = String.valueOf(symbols.charAt(i));
+        int index = 0;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (index < symbols.length()) {
+                    game[i][j] = String.valueOf(symbols.charAt(index));
+                    index++;
+                } else {
+                    game[i][j] = "";
+                }
+            }
         }
         return game;
+    }
+
+    public static int[] getUserInput() throws NumberFormatException {
+        Scanner input = new Scanner(System.in);
+        int[] values = new int[2];
+        boolean validInput = false;
+        do {
+            try {
+                String[] userInput = input.nextLine().split(" ");
+                values[0] = Integer.parseInt(userInput[0]);
+                values[1] = Integer.parseInt(userInput[1]);
+                if ((values[0] >= 1 && values[0] <= 3 && (values[1] >= 1 && values[1] <= 3))) {
+                    validInput = true;
+
+                } else {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("You should enter numbers!");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("wrong format");
+            }
+
+        } while (!validInput);
+        normalizeUserInput(values);
+        return values;
+    }
+
+    public static void normalizeUserInput(int[] values) {
+        values[0]--;
+        values[1]--;
+
     }
 }
 	
